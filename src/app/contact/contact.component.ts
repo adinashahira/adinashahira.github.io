a55a6a642d1d2dde7a07387a933319ee0e1dfb03
Nur Adina Shahira <aadinashahira@gmail.com>
2021-04-28 23:39:45 +0800

    completed contact and footer

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faLinkedinIn, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-contact',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  faLinkedinIn = faLinkedinIn;
+  faGithub = faGithub;
+  faTwitter = faTwitter;
   constructor() { }
 
   ngOnInit(): void {
