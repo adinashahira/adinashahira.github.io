2ce99d7ae1c4e2a8fe12effaee84c621b9acad0c
Nur Adina Shahira <aadinashahira@gmail.com>
2021-04-28 21:15:15 +0800

    added exp, edu and skills

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBriefcase, faSchool } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-about-me',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
-
+  faBriefcase = faBriefcase;
+  faSchool = faSchool;
   constructor() { }
 
   ngOnInit(): void {
